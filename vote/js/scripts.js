f268d24b544bb49f00a88a84aefcf6247a7529d0
Garrett Rowley and Robbie Kruszynski <garrett@>
2019-01-17 11:04:14 -0800

    adds 60+ condition

--- vote/js/scripts.js
+++ vote/js/scripts.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
   var are18 = parseInt(prompt("Input your age."));
 
-  if (are18>18){
+  if (are18 > 18 && are18 < 60){
     $("#canVote").show();
   } else if (are18 < 18) {
     $("#canNotVote").show();
-  } else if (are18 == 18) {
+  } else if (are18 === 18) {
   alert("welcome to adulthood");
+  $("#canVote").show();
+} else if (are18 === 60) {
+  alert("you're getting pretty old");
   $("#canVote").show();
   } else {
   alert("Not a valid age.");
